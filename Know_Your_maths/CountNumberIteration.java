9cbdeca6a4c157b2fa45f26b6fb1069c5d664bf0
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-09 19:09:28 +0530

    Update CountNumberIteration.java

--- Know_Your_maths/CountNumberIteration.java
+++ Know_Your_maths/CountNumberIteration.java
@@ -2,18 +2,18 @@ package Know_Your_maths;
 import java.util.Scanner;
 public class CountNumberIteration {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int g= sc.nextInt();
-        m(g);
+         Scanner sc= new Scanner(System.in);
+        int output= sc.nextInt();
+        count(output);
     }
-    static void m(int n){
-        int finditeration=0;
-        while(n!=0){
-            int sum=n%10;
-            finditeration++;
-            n=n/10;
-
+    static void count(int k){
+        int duplicate=k;
+        int Count=0;
+        while(k!=0){
+            int Number=k%10;
+            Count++;
+            k=k/10;
         }
-        System.out.println("The number length is "+finditeration);
-    }
+       System.out.println("The count of iteration in number "+duplicate+" is "+Count);
+   }
 }
