f08570ca8900ed6566378f171baf18be1bf231fa
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-09 18:36:37 +0530

    Add files via upload

---
+++ Know_Your_maths/Armstrong_number.java
@@ -0,0 +1,27 @@
+package Know_Your_maths;
+
+import java.util.Scanner;
+
+public class Armstrong_number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int output=sc.nextInt();
+        armstrong(output);
+    }
+    static void armstrong(int n){
+        int duplicate=n;
+        int sum=0;
+        while (n!=0){
+            int rem=n%10;
+            sum=sum+rem*rem*rem;
+            n=n/10;
+        }
+        System.out.println(sum);
+        if(duplicate==sum){
+            System.out.println("This is a armstrong number");
+        }
+        else {
+            System.out.println("This is not a armstrong number");
+        }
+    }
+}
