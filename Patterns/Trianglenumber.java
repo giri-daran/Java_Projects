500289976fd72ff363de5fe8070c8296befb4468
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-10 17:56:57 +0530

    Update Trianglenumber.java

--- Patterns/Trianglenumber.java
+++ Patterns/Trianglenumber.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Trianglenumber {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int h= sc.nextInt();
-        c(h);
+        int output= sc.nextInt();
+        TriangleNumber(output);
     }
-    static void c(int t){
+    static void TriangleNumber(int t){
         for (int i=1;i<=t;i++){
             for (int j=1;j<=i;j++){
                 System.out.print(j);
