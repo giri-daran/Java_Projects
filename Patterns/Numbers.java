5f67e84a567982089517188d6bd00790a0d910bd
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-11 17:38:35 +0530

    Update Numbers.java

--- Patterns/Numbers.java
+++ Patterns/Numbers.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Numbers {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int r=sc.nextInt();
-        y(r);
+        int output=sc.nextInt();
+        NumberTriangle(output);
     }
-    static void y(int n){
+    static void NumberTriangle(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(i);
