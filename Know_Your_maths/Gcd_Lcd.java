73b6eaca3c0de7e79fe33ebed5e8ebab1a7d377e
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-13 18:17:27 +0530

    Add files via upload

---
+++ Know_Your_maths/Gcd_Lcd.java
@@ -0,0 +1,19 @@
+package Know_Your_maths;
+
+public class Gcd_Lcd {
+    public static void main(String[] args) {
+
+    }
+    public static void yy(int a , int b){
+       int mini =0;
+        if(a>b){
+            mini=b;
+        } else{
+            mini =a;
+        }
+        for(int i=1;i<=mini; i++){
+            //System.out.println(i);
+            
+        }
+    }
+}
