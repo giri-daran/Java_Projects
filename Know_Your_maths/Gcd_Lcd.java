de590b0dbf2529e23ffec37828e96ccaf18aed6e
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-13 18:22:59 +0530

    Update Gcd_Lcd.java

--- Know_Your_maths/Gcd_Lcd.java
+++ Know_Your_maths/Gcd_Lcd.java
@@ -1,10 +1,14 @@
 package Know_Your_maths;
+import java.util.Scanner;
 
 public class Gcd_Lcd {
     public static void main(String[] args) {
-
+        Scanner sc= new Scanner(System.in);
+        int ou=sc.nextInt();
+        int ou2=sc.nextInt();
+        gcdlcd(ou,ou2);
     }
-    public static void yy(int a , int b){
+    public static void gcdlcd(int a , int b){
        int mini =0;
         if(a>b){
             mini=b;
