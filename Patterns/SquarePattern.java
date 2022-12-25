5d9e541e7e816240d66847cc529efd6538bfb111
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-25 16:27:02 +0530

    Update and rename Square.java to SquarePattern.java

--- Patterns/SquarePattern.java
+++ Patterns/SquarePattern.java
@@ -2,19 +2,19 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Square {
+public class SquarePattern {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int g= sc.nextInt();
-        sq(g);
+        int o=sc.nextInt();
+        m(o);
     }
-    static void sq(int y){
-        for(int i=1;i<=y;i++){
+    static void m(int n){
+        for (int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
-            for(int k=0;k<=(y-i);k++){
-                System.out.print("*");
+            for(int k=1;k<=(n-i);k++){
+                System.out.print("* ");
             }
             System.out.println();
         }
