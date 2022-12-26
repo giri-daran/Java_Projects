afaa62c2c3e2be0ff6cb19372cc0e408f3dc723d
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-26 16:45:06 +0530

    Add files via upload

---
+++ PatternsAlphabets/AphabetConeReverse.java
@@ -0,0 +1,30 @@
+package PatternsAlphabets;
+import java.util.Scanner;
+public class AphabetConeReverse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int h=sc.nextInt();
+        u(h);
+    }
+    static void u(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=n-1;j>=i-1;j--){
+                System.out.print((char)('A' + j));
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+*
+* input
+5
+* output
+E
+E D
+E D C
+E D C B
+E D C B A
+
+* */
