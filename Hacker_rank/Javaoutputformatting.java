1af417aaad375f01491b519871bad62269c5c69c
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-27 10:25:55 +0530

    Update Javaoutputformatting.java

--- Hacker_rank/Javaoutputformatting.java
+++ Hacker_rank/Javaoutputformatting.java
@@ -8,8 +8,8 @@ public class Javaoutputformatting {
         System.out.println("================================");
         for(int i=0;i<3;i++){
             String s1=sc.next();
-            int x=sc.nextInt();
-            System.out.printf("%-15s%03d \n",s1,x);
+            int output=sc.nextInt();
+            System.out.printf("%-15s%03d \n",s1,output);
             //Complete this line
         }
         System.out.println("================================");
