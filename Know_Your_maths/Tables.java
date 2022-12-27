4c1c508b3483affd4fc1766eddbeb18d4eb311bd
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-27 10:25:06 +0530

    Update Tables.java

--- Know_Your_maths/Tables.java
+++ Know_Your_maths/Tables.java
@@ -8,9 +8,9 @@ public class Tables {
         Scanner sc= new Scanner(System.in);
         int output1=sc.nextInt();
         int output2= sc.nextInt();
-        tables(output1,output2);
+        Tables(output1,output2);
     }
-    static void tables(int y, int u){
+    static void Tables(int y, int u){
         for(int i=1;i<=u;i++){
             int value=y*i;
             System.out.println(y +" X " +i +" = "+value);
