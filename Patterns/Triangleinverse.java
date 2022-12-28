961d867d1a8aed439074e6b03a9d674980158f22
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-28 10:55:58 +0530

    Update Triangleinverse.java

--- Patterns/Triangleinverse.java
+++ Patterns/Triangleinverse.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Triangleinverse {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int r= sc.nextInt();
-        u(r);
+        int output= sc.nextInt();
+        Triangleinverse(output);
     }
-    static void u(int y){
+    static void Triangleinverse(int y){
         for(int i=1;i<=y;i++){
             for(int j=1;j<=(y-i)+1;j++){
                 System.out.print("*");
