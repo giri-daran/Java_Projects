60282415adde43cca72e5f230a2687f3e3fec0c2
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-28 10:54:50 +0530

    Update Triangulate.java

--- Patterns/Triangulate.java
+++ Patterns/Triangulate.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Triangulate {
     public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
-     int h=sc.nextInt();
-     yy(h);
+     int output=sc.nextInt();
+     Triangulate(output);
     }
-    static void yy(int p){
+    static void Triangulate(int p){
         for(int i=1;i<=p;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
