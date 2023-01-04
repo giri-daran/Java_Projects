cfdaedc6b5d2de62f7636c0f1e7c39678c7d9559
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-01-04 10:19:14 +0530

    Add files via upload

---
+++ Know_Your_maths/FahrenheitToCelcius.java
@@ -0,0 +1,15 @@
+package Know_Your_maths;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelcius {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        float u=sc.nextFloat();
+        conertfartocel(u);
+    }
+    static void conertfartocel(float n){
+        float s= ((n-32)*5)/9;  //the formula for coverting fahrenheit to celcius
+        System.out.println("the temperation from fahrenhiet to celcius is "+s);
+    }
+}
