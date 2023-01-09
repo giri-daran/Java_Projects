9a7e19adfe7c1cd20d6e72f4336105a1dcb32749
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-01-09 12:50:00 +0530

    Update PrintNumberWithoutAnyLoops.java

--- BasicRecursiveProblems/PrintNumberWithoutAnyLoops.java
+++ BasicRecursiveProblems/PrintNumberWithoutAnyLoops.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class PrintNumberWithoutAnyLoops {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int o=sc.nextInt();
-        PrintNoWithRecursive(o);
+        int output=sc.nextInt();
+        PrintNoWithRecursive(output);
     }
     static void PrintNoWithRecursive(int n){
         if(n==1){
