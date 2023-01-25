7a9f4d1ccab3ce61dcc592c3ebbffaf9fec278ea
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-01-25 11:12:18 +0530

    Update FindThegratestNumber.java

--- Know_Your_maths/FindThegratestNumber.java
+++ Know_Your_maths/FindThegratestNumber.java
@@ -1,3 +1,32 @@
+package Know_Your_maths;
+import java.util.Scanner;
+public class GreatestNo {
+   public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+     System.out.println("The program will take 3 values N, O, P , Lets start from N ");
+     System.out.println("Enter the value of N ");
+     int N=sc.nextInt();
+     System.out.println("Enter the value of O ");
+     int O=sc.nextInt();
+     System.out.println("Enter the value of P ");
+     int P=sc.nextInt();
+     m(N,O,P);
+    }
+   static void m(int n,int o, int p){
+    if(n>o && n>p){
+      System.out.println("the value of N is the greatest of all 3 numbers");
+    }
+     else if(o>n && o>p){
+      System.out.println("The value of O is the greatest of all 3 numbers");
+     }
+     else if(p>n && p>o){
+      System.out.println("The value of P is the greatest of all 3 numbers");
 
+     }
+     else{
+      System.out.println("All 3 values are either equal or not distict");
+     }
+   }
+}
 
 
