6f94000df434e261c3e35865d94c5f446fcbb1fd
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-01-26 13:10:56 +0530

    Update Gcd_Lcd.java

--- Know_Your_maths/Gcd_Lcd.java
+++ Know_Your_maths/Gcd_Lcd.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Gcd_Lcd {
     public static void main(String[] args) {
+        System.out.println("Type the number to get the GCD and LCD - ");
         Scanner sc= new Scanner(System.in);
         int output=sc.nextInt();
         int output2=sc.nextInt();
@@ -21,3 +22,54 @@ public class Gcd_Lcd {
         }
     }
 }
+
+/*
+input:-
+Type the number to get the GCD and LCD - 
+45
+76
+output:-
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+3334
+353637
+38
+39
+40
+41
+42
+4344
+45
+
+*/
