4ebb34233fcd964e70941904aa8226a1071dc6c1
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-02-05 19:22:22 +0530

    Update Palindorme.java

--- Know_Your_maths/Palindorme.java
+++ Know_Your_maths/Palindorme.java
@@ -1,10 +1,9 @@
 package Know_Your_maths;
-
 import java.util.Scanner;
-
 public class Palindorme {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number to check for palindrome ");
         int a=sc.nextInt();
         palindrome(a);
     }
@@ -24,3 +23,17 @@ public class Palindorme {
         }
     }
 }
+
+/*
+input:-
+Enter the number to check for palindrome 
+567
+---------------------------------------------
+Enter the number to check for palindrome 
+121
+
+output:-
+765 is not a palindorme
+-----------------------------------------------
+121 is a palindrome
+*/
