1d127ac93b082c696640ce19b9a86deac9dff1cd
giri-daran <114486252+giri-daran@users.noreply.github.com>
2023-02-05 19:14:46 +0530

    Update Tables.java

--- Know_Your_maths/Tables.java
+++ Know_Your_maths/Tables.java
@@ -1,12 +1,12 @@
 package Know_Your_maths;
-
 import java.util.Scanner;
-
 public class Tables {
     public static void main(String[] args) {
 
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number for Tables ");
         int output1=sc.nextInt();
+        System.out.println("Enter the number of limit in this Tables you need ");
         int output2= sc.nextInt();
         Tables(output1,output2);
     }
@@ -14,7 +14,7 @@ public class Tables {
         for(int i=1;i<=u;i++){
             int value=y*i;
             System.out.println(y +" X " +i +" = "+value);
-
         }
     }
 }
+
